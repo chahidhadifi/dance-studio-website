a42b27de1c39cc23054849ac8c3a9264d1a5c29d
Chahid Hadifi <chahidhadifi@gmail.com>
2024-02-05 22:31:34 +0100

    update header

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,37 @@
-console.log("Hello World!");
+// body
+const body = document.querySelector("body");
+
+// toggle
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+const headerToggle = document.querySelector(".header__toggle");
+let isOpened = false;
+
+// menu
+const menu = document.getElementById("menu");
+
+headerToggle.addEventListener("click", () => {
+  if (isOpened) {
+    isOpened = false;
+    // toggle
+    openMenu.setAttribute("style", "display: block");
+    closeMenu.setAttribute("style", "display: none");
+    // menu
+    menu.classList.add("fade-out");
+    menu.classList.remove("fade-in");
+    menu.setAttribute("style", "display: none");
+    // body
+    body.setAttribute("style", "overflow: auto");
+  } else {
+    isOpened = true;
+    // toggle
+    openMenu.setAttribute("style", "display: none");
+    closeMenu.setAttribute("style", "display: block");
+    // menu
+    menu.classList.add("fade-in");
+    menu.classList.remove("fade-out");
+    menu.setAttribute("style", "display: block");
+    // body
+    body.setAttribute("style", "overflow: hidden");
+  }
+});
